14c999fc1cd732007a44729ed536e279e437d214
Gaby137 <genrriquezsanchez@alumno.unlam.edu.ar>
2023-06-11 19:18:42 -0300

    se movio la logica del qr en un metodo en usuarioModel

--- controller/PerfilJugadorController.php
+++ controller/PerfilJugadorController.php
@@ -3,7 +3,8 @@ include_once('./model/UsuarioModel.php');
 include_once('./model/RankingModel.php');
 require_once('./phpqrcode/qrlib.php');
 
-class PerfilJugadorController {
+class PerfilJugadorController
+{
     private $renderer;
     private $usuarioModel;
     private $rankingModel;
@@ -15,30 +16,20 @@ class PerfilJugadorController {
         $this->rankingModel = $rankingModel;
     }
 
-    public function list() {
+    public function list()
+    {
         $id = $_GET['id'];
         $usuario = $this->usuarioModel->getUsuarioPorId($id);
 
         $resultado = $this->rankingModel->obtenerElRankingPorIdUsuario($id);
 
-
-        $url = 'http://localhost/tpFinal/perfilJugador/list?id=' . $id;
-
-// Ruta y nombre del archivo de imagen del código QR (puede ser un archivo PNG, JPG, etc.)
-        $archivoImagen = './public/img/qrcode.png';
-
-// Tamaño y nivel de corrección del código QR (0 = bajo, 1 = medio, 2 = alto, 3 = mejor)
-        $tamaño = 10;
-        $nivelCorreccion = 'L';
-
-// Generar el código QR
-        QRcode::png($url, $archivoImagen, $nivelCorreccion, $tamaño);
+        $qr = $this->usuarioModel->generarQr($id);
 
 // Mostrar la imagen del código QR
         $datos = array(
             'ranking' => $resultado[0],
             'usuario' => $usuario[0],
-            'qr' => '.'.$archivoImagen
+            'qr' => '.' . $qr
         );
         $this->renderer->render("perfilJugador", $datos);
     }
