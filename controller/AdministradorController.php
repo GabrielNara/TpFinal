b37236762698565726ebf84939d0e1647a41b272
GabrielNaran <102691523+GabrielNara@users.noreply.github.com>
2023-06-17 20:54:02 -0300

    .
    .

--- controller/AdministradorController.php
+++ controller/AdministradorController.php
@@ -35,18 +35,57 @@ class AdministradorController
 
     }
 
-    public function cantJugadores(){
+/*
+    public function cantJugadores()
+    {
         $this->redireccionamiento();
 
+
+
         $cantidadJugadores = $this->administradorModel->obtenerCantidadJugadores();
+        $jugadores = $cantidadJugadores[0]['cantidad'];
+
+        $contexto = array(
+            'jugadores' => $jugadores
+        );
+
+        $this->renderer->render("cantidadJugadores", $contexto);
+    }
+ */
+    public function cantJugadores($filtro = 'todos')
+    {
+        $this->redireccionamiento();
+
+        $filtroPeriodo = '';
+
+        switch ($filtro) {
+            case 'dia':
+                $filtroPeriodo = 'DATE(u.fecha_registro) = CURDATE()';
+                break;
+            case 'semana':
+                $filtroPeriodo = 'WEEK(u.fecha_registro) = WEEK(CURDATE())';
+                break;
+            case 'mes':
+                $filtroPeriodo = 'MONTH(u.fecha_registro) = MONTH(CURDATE())';
+                break;
+            case 'anio':
+                $filtroPeriodo = 'YEAR(u.fecha_registro) = YEAR(CURDATE())';
+                break;
+            default:
+                // Filtro "todos" (sin restricciones)
+                break;
+        }
+
+        $cantidadJugadores = $this->administradorModel->obtenerCantidadJugadores($filtroPeriodo);
 
         $contexto = array(
             'jugadores' => $cantidadJugadores
         );
 
-
-        $this->renderer->render("cantidadJugadores",$contexto);
+        $this->renderer->render("cantidadJugadores", $contexto);
     }
+
+
 
 
 
--- model/AdministradorModel.php
+++ model/AdministradorModel.php
@@ -16,18 +16,60 @@ class AdministradorModel
         return $this->database->querySelectFetchAssoc($query);
     }
 
-    public function obtenerCantidadJugadores(){
 
-        $query = "SELECT u.*
-               FROM usuarios u
-               INNER JOIN usuarios_roles ur ON u.id = ur.idUsuario
-               INNER JOIN roles r ON ur.idRol = r.id
-               WHERE r.id = 3";
+ /*
+    public function obtenerCantidadJugadores()
+    {
+        $query = "SELECT COUNT(*) AS cantidad FROM usuarios u
+              INNER JOIN usuarios_roles ur ON u.id = ur.idUsuario
+              INNER JOIN roles r ON ur.idRol = r.id
+              WHERE r.id = 3";
 
         return $this->database->querySelectFetchAssoc($query);
+    }
 
+ */
+    public function obtenerCantidadJugadores($filtroPeriodo = '')
+    {
+        $query = "SELECT COUNT(*) AS cantidad FROM usuarios u
+              INNER JOIN usuarios_roles ur ON u.id = ur.idUsuario
+              INNER JOIN roles r ON ur.idRol = r.id
+              WHERE r.id = 3";
+
+        if ($filtroPeriodo !== '') {
+            $query .= " AND " . $filtroPeriodo;
+        }
+
+        return $this->database->querySelectFetchAssoc($query);
     }
 
 
 
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
