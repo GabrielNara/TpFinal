1027950ad81f2cd6ed2936beb3e806227a103c7c
TomasAyerbe <tomayerbe@gmail.com>
2023-06-26 15:13:49 -0300

    Arreglo de trampitas cuando se crea usuario nuevo. Estadísticas y gráficos en PDF para la compra de trampitas. Actualización registros de la base de datos para la entrega final.

--- model/FormRegistroModel.php
+++ model/FormRegistroModel.php
@@ -15,59 +15,46 @@ class FormRegistroModel
 	{
 		$errores = [];
 		if (empty($datos['nombre'])) {
-			////throw new Exception('El campo nombre no puede estar vacío.');
 			$errores['errorNombre'] = 'El campo nombre no puede estar vacío.';
 		}
 		if (empty($datos['apellido'])) {
-			//throw new Exception('El campo apellido no puede estar vacío.');
 			$errores['errorApellido'] = 'El campo apellido no puede estar vacío.';
 		}
 		if (empty($datos['username'])) {
-			//throw new Exception('El campo nombre de usuario no puede estar vacío.');
 			$errores['errorUsername'] = 'El campo nombre de usuario no puede estar vacío.';
 		}
 		$query = "SELECT * FROM usuarios WHERE nombre_usuario = '{$datos['username']}'";
 		$result = $this->database->query($query);
 		if (count($result) > 0) {
-			//throw new Exception('Ya existe un jugador con ese nombre de usuario.');
 			$errores['errorUsername'] = 'Ya existe un jugador con ese nombre de usuario.';
 		}
 		if (empty($datos['localidad'])) {
-			//throw new Exception('El campo país y ciudad no puede estar vacío.');
 			$errores['errorPaisCiudad'] = 'El campo país y ciudad no puede estar vacío.';
 		}
 		if (empty($datos['sexo'])) {
-			//throw new Exception('El campo sexo no puede estar vacío.');
 			$errores['errorSexo'] = 'El campo sexo no puede estar vacío.';
 		}
 		if (empty($datos['fnacimiento'])) {
-			//throw new Exception('El campo año de nacimiento no puede estar vacío.');
 			$errores['errorAnoNacimiento'] = 'El campo año de nacimiento no puede estar vacío.';
 		}
 		if (empty($datos['email'])) {
-			//throw new Exception('El campo email no puede estar vacío.');
 			$errores['errorEmail'] = 'El campo email no puede estar vacío.';
 		}
 		$query = "SELECT * FROM usuarios WHERE email = '{$datos['email']}'";
 		$result = $this->database->query($query);
 		if (count($result) > 0) {
-			//throw new Exception('Ya existe un jugador con ese correo electrónico.');
-			$errores['errorEmail'] = 'El campo email no puede estar vacío.';
+			$errores['errorEmail'] = 'El email ya fue utilizado.';
 		}
 		if (empty($datos['password'])) {
-			//throw new Exception('El campo contraseña no puede estar vacío.');
 			$errores['errorContrasena'] = 'El campo contraseña no puede estar vacío.';
 		}
 		if (empty($datos['cpassword'])) {
-			//throw new Exception('El campo repetir contraseña no puede estar vacío.');
 			$errores['errorRepetirContrasena'] = 'El campo repetir contraseña no puede estar vacío.';
 		}
 		if (empty($datos['foto_perfil'])) {
-			//throw new Exception('Debe subir una foto de perfil.');
 			$errores['errorFotoPerfil'] = 'Debe subir una foto de perfil.';
 		}
 		if (!filter_var($datos['email'], FILTER_VALIDATE_EMAIL)) {
-			//throw new Exception('El email no es válido.');
 			$errores['errorEmail'] = 'El email no es válido.';
 		}
 
@@ -123,7 +110,6 @@ VALUES ('$nombre','$apellido','$email','$contrasena','$anionacimiento','$sexo','
 	public function validarCorreo()
 	{
 		// Obtener el token desde la URL// obtengo el token desde una consulta a la bd
-
 		$token = $_GET['token'];
 
 		$query = "SELECT * FROM usuarios WHERE token = '$token'";
