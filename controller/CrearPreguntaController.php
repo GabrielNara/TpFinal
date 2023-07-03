b2addb6089a8b6c4bd1b26f153d5fd34ae0241fa
Gaby137 <genrriquezsanchez@alumno.unlam.edu.ar>
2023-07-03 17:32:22 -0300

    bug en crearPregunta

--- controller/CrearPreguntaController.php
+++ controller/CrearPreguntaController.php
@@ -21,20 +21,22 @@ class CrearPreguntaController
     }
 
     public function crearPregunta(){
+        $cat = $this->crearPreguntaModel->obtenerCategorias();
         $datos = array(
             'pregunta' => $_POST['pregunta'],
             'opcionA' => $_POST['opcionA'],
             'opcionB' => $_POST['opcionB'],
             'opcionC' => $_POST['opcionC'],
             'opcionD' => $_POST['opcionD'],
-            'categoria' => $_POST['categoria']
+            'categoria' => $_POST['categoria'],
 
         );
         $errores = $this->crearPreguntaModel->validarCreacionPregunta($datos);
 
         $contexto = array(
             'datos' => $datos,
-            'errores' => $errores
+            'errores' => $errores,
+            'categorias' => $cat
         );
 
         if (!empty($errores)) $this->renderer->render("crearPregunta", $contexto); else $this->renderer->render("confirmarPregunta");
